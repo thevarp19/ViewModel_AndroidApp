2d5c9e8cdd1d798e8728c08040f7382b31861380
Yerlan Jumabayev <thevarp18@gmail.com>
2023-11-11 01:52:01 +0600

    Handle last round of game

--- app/src/main/java/com/example/unscramble/ui/GameViewModel.kt
+++ app/src/main/java/com/example/unscramble/ui/GameViewModel.kt
@@ -2,6 +2,7 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
+import com.example.unscramble.data.MAX_NO_OF_WORDS
 import com.example.unscramble.data.SCORE_INCREASE
 import com.example.unscramble.data.allWords
 import kotlinx.coroutines.flow.MutableStateFlow
@@ -51,6 +52,7 @@ class GameViewModel : ViewModel() {
             return shuffleCurrentWord(currentWord)
         }
     }
+
     fun checkUserGuess() {
 
         if (userGuess.equals(currentWord, ignoreCase = true)) {
@@ -62,6 +64,29 @@ class GameViewModel : ViewModel() {
         }
         updateUserGuess("")
     }
+    private fun updateGameState(updatedScore: Int) {
+        if (usedWords.size == MAX_NO_OF_WORDS){
+            //Last round in the game, update isGameOver to true, don't pick a new word
+            _uiState.update { currentState ->
+                currentState.copy(
+                    isGuessedWordWrong = false,
+                    score = updatedScore,
+                    isGameOver = true
+                )
+            }
+        } else{
+            // Normal round in the game
+            _uiState.update { currentState ->
+                currentState.copy(
+                    isGuessedWordWrong = false,
+                    currentScrambledWord = pickRandomWordAndShuffle(),
+                    currentWordCount = currentState.currentWordCount.inc(),
+                    score = updatedScore
+                )
+            }
+        }
+    }
+
 
     fun updateUserGuess(guessedWord: String){
         userGuess = guessedWord
